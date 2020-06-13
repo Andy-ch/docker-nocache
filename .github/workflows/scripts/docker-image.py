e74a429edcadf7517d6a70f51a2bed79e50c9d4c
Andy-ch <andy.v.che@gmail.com>
2020-06-13 14:41:18 +0400

    Retrieve tags from Docker Hub API output

--- .github/workflows/scripts/docker-image.py
+++ .github/workflows/scripts/docker-image.py
@@ -3,9 +3,18 @@
 import requests
 
 
+def get_tags(api_link, result=[]):
+    try:
+        data = requests.get(api_link).json()
+    except requests.exceptions.MissingSchema:
+        return result
+    tags = result + [result['name'] for result in data['results']]
+    return get_tags(data['next'], tags)
+
+
 def process_image(image):
-    data = requests.get(f'https://hub.docker.com/v2/repositories/{image}/tags').json()
-    print(data)
+    tags = get_tags(f'https://hub.docker.com/v2/repositories/{image}/tags')
+    print(tags)
 
 
 def main():
